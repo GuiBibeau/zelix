9d74e5ed9c55d3460dc12aaf078313acfa76e8ed
guibibeau <g.bibeaulaviolette@gmail.com>
2025-12-29 22:50:20 +0200

    move Helius RPC endpoint to env file

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { autoDiscover, SolanaClientConfig } from "@solana/client";
+import { SolanaProvider } from "@solana/react-hooks";
+
+const config: SolanaClientConfig = {
+	endpoint: process.env.NEXT_PUBLIC_SOLANA_RPC_ENDPOINT!,
+	walletConnectors: autoDiscover(),
+};
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
@@ -10,7 +17,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 			enableSystem
 			disableTransitionOnChange
 		>
-			{children}
+			<SolanaProvider config={config}>{children}</SolanaProvider>
 		</ThemeProvider>
 	);
 }
